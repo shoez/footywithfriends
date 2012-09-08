6a86b63de173a9314db18996b1af06ef7c24390a
Henrik Pettersson <h.pettersson@gmail.com>
2012-09-08 18:23:35 +0100

    Setup page handlers

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,31 @@
 footy = {
-	init: function() {
-		// Get user
+    init: function () {
+        // Get user
+        //this.getUser();
 
-		// Init listeners
-		this.setupListeners();
-	}
+        // Setup submit handler
+        $("#invite-friends").submit(function(e) {
+            e.stopPropagation();
+            alert("invite friends YY");
+            $.mobile.changePage( "#page-match", { transition: "slide"} );
+            return false;
+        });
+
+        // Setup page handlers
+        $( document ).delegate("#page-matches", "pagebeforecreate", function() {
+            // Get list of pages
+        });
+        $( document ).delegate("#page-friends", "pagebeforecreate", function() {
+            // Get list of friends
+        });
+
+        // Init listeners
+        this.setupListeners();
+    }
 };
 
 footy.setupListeners = function() {
-	var id = 100;
+    var id = 100;
     var client = new Faye.Client('http://localhost:8000/faye');
 
     var Logger = {
@@ -25,7 +42,9 @@ footy.setupListeners = function() {
     //client.addExtension(Logger);
 
     client.subscribe('/competition/' + id + '/events/quiztime', function (data) {
-      console.log('Now show the quiz', data);
+        console.log('Now show the quiz', data);
+        // Show quiz dialog
+        $.mobile.changePage( "#dialog-quiz", {} );
     });
 
     console.log('subscribe to game start');
